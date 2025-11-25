241a7dabed65634df88f667bc2aedeaaacd3c36c
Andrey Zhuk <an.zhukovskii@gmail.com>
2025-11-25 23:04:17 +0300

    add two input

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,6 +9,8 @@ import '@fontsource/roboto/700.css';
 import './App.css'
 import AppBar from '@mui/material/AppBar';
 import ButtonAppBar from './AppBar.tsx';
+import { Button, Container, InputAdornment, Stack, TextField, Typography } from '@mui/material';
+import { AccountCircle } from '@mui/icons-material';
 
 
 
@@ -21,10 +23,43 @@ function App() {
 			<AppBar/>
 			<ButtonAppBar />
       
-			<div style={{marginTop: "100px"}}>
-				
-			</div>
-
+			<div style={{marginTop: "100px"}} />
+			
+			<Container maxWidth={"sm"}>
+				<Typography variant='h4' gutterBottom> Login </Typography>
+				<Stack spacing={2}>
+					<TextField
+						onChange={(e)=> console.log(e.target.value)}  
+						label="email" 
+						variant="filled" 
+						slotProps={{
+          					input: {
+								startAdornment: (
+									<InputAdornment position="start">
+										<AccountCircle />
+									</InputAdornment>
+								),
+          					},
+        				}}
+					/>
+					<TextField  
+						label="Pasword" 
+						type="password"
+						variant="filled" 
+						slotProps={{
+							input: {
+								startAdornment: (
+									<InputAdornment 
+										position="start">
+										<AccountCircle />
+									</InputAdornment>
+								),
+							},
+						}}
+					/>
+					<Button variant='contained'>Login</Button>
+				</Stack>
+			</Container>
 		</>
 	);
 }
